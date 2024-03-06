d011286eb375ad17db797ef049021e23f0f05be6
Cleidy Gil <51685060+cleidygil@users.noreply.github.com>
2024-03-06 17:42:12 -0400

    BUGFIX check de productos eliminado

--- src/app/core/supplies/components/new-product/new-product.component.ts
+++ src/app/core/supplies/components/new-product/new-product.component.ts
@@ -1,12 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AllStore, MethosdParams, MyStoreParams } from 'src/app/core/store/interfaces/store';
-import { StoreService } from 'src/app/core/store/services/store.service';
-import { GlobalService } from 'src/app/global/services/global.service';
-import { SnackbarService } from 'src/app/global/services/snackbar.service';
-import { SuppliesService } from '../../services/supplies.service';
-import { ParamsGlobal } from '../../interfaces/supplies';
+
 
 @Component({
   selector: 'app-new-product',
@@ -15,14 +9,6 @@ import { ParamsGlobal } from '../../interfaces/supplies';
 })
 export class NewProductComponent {
   constructor() { }
-  checked = new FormGroup({
-    check: new FormControl<boolean>(false)
-  })
-  productNew = new FormGroup({
-    name: new FormControl<any>('', [Validators.required]),
-    barcode: new FormControl<any>('', [Validators.required]),
-  })
-
   selectedFile: any = null;
   myFiles: any[] = [];
   format: any = [];
@@ -32,20 +18,6 @@ export class NewProductComponent {
     file: new FormControl(''),
     url: new FormControl(''),
   })
-
-
-  onSubmitProduct() {
-    const valor = this.productNew.value
-    let body = {
-      name: valor?.name,
-      barcode: valor?.barcode,
-      authorization: this.checked.value.check
-    }
-    console.log(body)
-
-  }
-
-
   seleccionarArchivo(event: any) {
     console.log(this.files.value)
     const file = event.target.files[0];
@@ -56,9 +28,9 @@ export class NewProductComponent {
       const result = reader.result as string;
       const dataUrl = event.target?.result as string
       const isImageType = file.type.startsWith('image/');
-      let imageUrl:any
+      let imageUrl: any
       if (isImageType) {
-      //  imageUrl = this.convertirBase64AUrl(dataUrl);
+        //  imageUrl = this.convertirBase64AUrl(dataUrl);
         this.myFiles = [{ name: file.name, imageData: result, preview: result }];
       } else {
         this.myFiles = [{ name: file.name, imageData: result, preview: null }];
@@ -69,12 +41,12 @@ export class NewProductComponent {
           image_data: file.imageData
         }
       })
-      
-     
+
+
       this.format = files
     };
     reader.readAsDataURL(file);
-   
+
   }
   resetFile(indice: number): void {
     this.myFiles = this.myFiles.filter(
@@ -82,12 +54,12 @@ export class NewProductComponent {
     );
     this.format = this.format.filter((fil: string, i: number) => i !== indice);
   }
-  convertirBase64AUrl(base64Data: string):string {
+  convertirBase64AUrl(base64Data: string): string {
     console.log(base64Data)
-    const byteCharacters =  JSON.parse(window.atob(base64Data));
+    const byteCharacters = JSON.parse(window.atob(base64Data));
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
-        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
     const blob = new Blob([byteArray], { type: 'image/jpeg' });
@@ -96,5 +68,5 @@ export class NewProductComponent {
     const objectURL = URL.createObjectURL(blob);
     console.log(objectURL)
     return objectURL;
-}
+  }
 }
