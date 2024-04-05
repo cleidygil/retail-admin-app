4a2f849302cd80602a55cea2798f9875452c1b05
Cristhian Cotte <97977605+CristhianCotte@users.noreply.github.com>
2024-04-05 16:17:39 -0400

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    API_URL:  'http://192.168.196.251:7000',
-    // API_URL: 'http://147.182.234.154'
-
+    // API_URL:  'http://192.168.196.251:7000',
+    API_URL: 'http://147.182.234.154'
 };
